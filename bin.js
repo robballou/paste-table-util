0baa32e916988b72b9179c60a65e313a78b3e79e
Rob Ballou <rob.ballou@bombbomb.com>
2020-10-20 09:46:58 -0600

    Updated dependencies

--- bin.js
+++ bin.js
@@ -1,21 +1,20 @@
 #!/usr/bin/env node
-var program = require('commander');
-var pasteTable = require('./index.js');
+const pasteTable = require('./index.js');
 
 program = pasteTable.program();
 program
   .parse(process.argv);
 
-var data = [];
+const data = [];
 process.stdin.setEncoding('utf8');
 process.stdin.on('readable', function() {
-  var chunk = process.stdin.read();
+  const chunk = process.stdin.read();
   if (chunk !== null) {
     data.push(chunk);
   }
 });
 
 process.stdin.on('end', function() {
-  var output = pasteTable.commandLine(data, program);
+  const output = pasteTable.commandLine(data, program);
   console.log(output);
 });
